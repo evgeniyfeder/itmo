d3228f6cd347f19dcb6dfb18531fb2e878a739d8
evgeniyfeder <dzaba67@yandex.ru>
2018-07-04 13:13:52 +0300

    Update paradigms structure

--- paradigms/java/dz-5/dz-5.1/src/ru/itmo/ctddev/feder/expression/Expression.java
+++
@@ -1,7 +0,0 @@
-package ru.itmo.ctddev.feder.expression;
-
-/* Expression interface */
-public interface Expression {
-    /* Count expression from one integer argument */
-    int evaluate(int x);
-} /* End of 'Expression' interface */
