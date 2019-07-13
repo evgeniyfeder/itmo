406db207807c22f6a24db26acf9eb90ed79607a7
evgeniyfeder <evgeniyfeder@github.com>
2019-07-13 21:40:56 +0300

    Add 3rd mt task

---
+++ mt/lab3/a.cpp
@@ -0,0 +1,28 @@
+int pow(int a1, int a2) {
+{
+  int a = a1;
+  int n = a2;
+  if ((n==0) && (true)) {
+    return 1;
+  }
+}
+{
+  int a = a1;
+  int n = a2;
+  if ((n%2==1) && (true)) {
+    return pow(a,n-1)*a;
+  }
+}
+{
+  int a = a1;
+  int n = a2;
+  if ((true) && (true)) {
+    return pow(pow(a,n/2),2);
+  }
+}
+}
+
+int main() {
+    pow(5, 10);
+}
+
